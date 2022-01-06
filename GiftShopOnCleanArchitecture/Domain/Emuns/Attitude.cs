b7c88ee891dc630c008dc17def08ba570521fd4f
KolyaNu4 <mykola.andrietc@gmail.com>
2022-01-06 17:07:06 +0200

    Some additions

---
+++ GiftShopOnCleanArchitecture/Domain/Emuns/Attitude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Emuns
+{
+    public enum Attitude
+    {
+        Like,
+        Dislike,
+        None,
+    }
+}
---
+++ GiftShopOnCleanArchitecture/Infrastracture/Persistence/Configurations/RelationshipConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastracture.Persistence.Configurations
+{
+    public class RelationshipConfiguration : IEntityTypeConfiguration<Relationship>
+    {
+        public void Configure(EntityTypeBuilder<Relationship> builder)
+        {
+            builder.HasKey(x => new { x.UserId, x.BoxId });
+            builder.HasOne(r => r.User)
+                .WithMany(u => u.Relationship)
+                .HasForeignKey(r => r.UserId);
+            builder.HasOne(r => r.Box)
+                .WithMany(b => b.Relationship)
+                .HasForeignKey(r => r.BoxId);
+        }
+    }
+}
