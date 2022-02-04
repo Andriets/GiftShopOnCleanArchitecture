599963211e43bdfd31436dd949ec2c014aca5fd2
KolyaNu4 <mykola.andrietc@gmail.com>
2022-02-04 17:59:00 +0200

    Tag CRUD

---
+++ GiftShopOnCleanArchitecture/Application/Tag/Commands/UpdateTag/UpdateTagCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces;
+using Application.Tags.Commands.UpdateTag;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Tags.Commands.UpdateTag
+{
+    public class UpdateTagCommandHandler : IRequestHandler<UpdateTagCommand, Guid>
+    {
+        private readonly IAppDbContext _context;
+
+        public UpdateTagCommandHandler(IAppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public async Task<Guid> Handle(UpdateTagCommand request, CancellationToken cancellationToken)
+        {
+            var tag = _context.Tags.FirstOrDefault(t => t.Id == request.TagId);
+
+            tag.TagName = request.NewTagName;
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return tag.Id;
+        }
+    }
+}
---
+++ GiftShopOnCleanArchitecture/Application/Tag/Queries/GetAllTags/GetAllTagsQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tags.Queries.GetAllTags
+{
+    public class GetAllTagsQuery : IRequest<IEnumerable<TagDTO>>
+    {
+        public string KeyWord { get; set; }
+    }
+}
